0ade24dc5be7035b9e94ffe646145bc0b70f2496
Michal Cyprian <mcyprian@redhat.com>
2017-07-28 16:14:18 +0200

    Add unversioned shebangs check
    This check searches for #!/usr/bin/python and /usr/bin/env python
    shebangs inside RPM packages. If some of these shebangs is present,
    check fails and the complete list of problematic files is stored
    into the log.
    
    Resolves #28.

---
+++ taskotron_python_versions/unversioned_shebangs.py
@@ -0,0 +1,101 @@
+import sys
+import libarchive
+
+from .common import log, write_to_artifact
+
+MESSAGE = """These RPMs contains problematic shebang in some of the scripts:
+{}
+This is discouraged and should be avoided. Please check the shebangs
+and use either `#!/usr/bin/python2` or `#!/usr/bin/python3`.
+"""
+
+# TODO: update to real doc relevant to shebangs
+INFO_URL = ' https://pagure.io/packaging-committee/issue/698'
+
+FORBIDDEN_SHEBANGS = ['#!/usr/bin/python', '#!/usr/bin/env python']
+
+if sys.version > '3':
+    def to_bytes(s):
+        return str.encode(s)
+else:
+    def to_bytes(s):
+        return s
+
+
+def matches(line, query):
+    return line == query or line.startswith(query + b' ')
+
+
+def get_problematic_files(archive, query):
+    """Search for the files inside archive with the first line
+    matching given query.
+    """
+
+    problematic = set()
+    with libarchive.file_reader(archive) as a:
+        for entry in a:
+            try:
+                first_line = next(entry.get_blocks(), '').splitlines()[0]
+            except IndexError:
+                continue
+            if matches(first_line, to_bytes(query)):
+                problematic.add(entry.pathname.lstrip('.'))
+
+    return problematic
+
+
+def get_scripts_summary(package):
+    """Collect problematic scripts data for given RPM package.
+    Content of archive is processed only package require
+    unversioned python binary.
+    """
+    scripts_summary = {key: set() for key in FORBIDDEN_SHEBANGS}
+
+    for key in scripts_summary.keys():
+        if to_bytes(key.split()[0][2:]) in package.require_names:
+            scripts_summary[key] = get_problematic_files(package.path, key)
+    return scripts_summary
+
+
+def task_unversioned_shebangs(packages, koji_build, artifact):
+    """Check if some of the binaries contains '/usr/bin/python'
+    shebang or 'usr/bin/env' shebang.
+    """
+    # libtaskotron is not available on Python 3, so we do it inside
+    # to make the above functions testable anyway
+    from libtaskotron import check
+
+    outcome = 'PASSED'
+
+    problem_rpms = {}
+    shebang_message = ''
+
+    for package in packages:
+        log.debug('Checking shebangs of {}'.format(package.filename))
+        problem_rpms[package.nvr] = get_scripts_summary(package)
+
+    log.debug("\n\nPROBLEM RPMS: {}".format(problem_rpms))
+    for package, pkg_summary in problem_rpms.items():
+        for shebang, scripts in pkg_summary.items():
+            if scripts:
+                outcome = 'FAILED'
+                shebang_message += \
+                    '{}\n * Scripts containing `{}` shebang:\n   {}'.format(
+                        package, shebang, '\n   '.join(scripts))
+
+    detail = check.CheckDetail(
+        checkname='python-versions.unversioned_shebangs',
+        item=koji_build,
+        report_type=check.ReportType.KOJI_BUILD,
+        outcome=outcome)
+
+    if outcome == 'FAILED':
+        detail.artifact = artifact
+        write_to_artifact(artifact, MESSAGE.format(shebang_message), INFO_URL)
+    else:
+        shebang_message = 'No problems found.'
+
+    log.info('python-versions.unversioned_shebangs {} for {}. {}'.format(
+        outcome, koji_build, shebang_message))
+
+    return detail
---
+++ test/functional/test_unversioned_shebangs.py
@@ -0,0 +1,60 @@
+import os
+import pytest
+
+from taskotron_python_versions.unversioned_shebangs import (
+    matches,
+    get_problematic_files,
+    get_scripts_summary,
+)
+from taskotron_python_versions.common import Package
+
+fixtures_dir = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))) + '/fixtures/'
+
+
+@pytest.mark.parametrize(('line', 'query', 'expected'), (
+    (b'#!/usr/bin/python', b'#!/usr/bin/python', True),
+    (b'#!/usr/bin/python ', b'#!/usr/bin/python', True),
+    (b'#!/usr/bin/python -I', b'#!/usr/bin/python', True),
+    (b'#!/usr/bin/python #comment', b'#!/usr/bin/python', True),
+    (b'#!/usr/bin/python3', b'#!/usr/bin/python', False),
+    (b'#!/usr/bin/python2', b'#!/usr/bin/python', False),
+    (b'#!/usr/bin/env python', b'#!/usr/bin/env python', True),
+    (b'#!/usr/bin/env python -I', b'#!/usr/bin/env python', True),
+    (b'#!/usr/bin/env python3', b'#!/usr/bin/env python', False),
+    (b'#!/usr/bin/env python2', b'#!/usr/bin/env python', False),
+    (b'#!/usr/bin/env perl', b'#!/usr/bin/env python', False),
+))
+def test_matches(line, query, expected):
+    assert matches(line, query) == expected
+
+
+@pytest.mark.parametrize(('archive', 'query', 'expected'), (
+    ('tracer-0.6.9-2.fc25.noarch.rpm',
+     '#!/usr/bin/python', {'/usr/bin/tracer'}),
+    ('python3-django-1.10.7-1.fc26.noarch.rpm', '#!/usr/bin/env python',
+     {'/usr/lib/python3.6/site-packages/django/bin/django-admin.py',
+      '/usr/lib/python3.6/site-packages/django/conf/project_template/manage.py-tpl'}),
+    ('python3-django-1.10.7-1.fc26.noarch.rpm', '#!/usr/bin/python', set()),
+    ('pyserial-2.7-6.fc25.noarch.rpm', '#!/usr/bin/python', set()),
+))
+def test_get_problematic_files(archive, query, expected):
+    assert get_problematic_files(fixtures_dir + archive, query) == expected
+
+
+@pytest.mark.parametrize(('path', 'expected'), (
+    ('tracer-0.6.9-2.fc25.noarch.rpm',
+     {'#!/usr/bin/python': {'/usr/bin/tracer'},
+      '#!/usr/bin/env python': set()}),
+    ('python3-django-1.10.7-1.fc26.noarch.rpm',
+     {'#!/usr/bin/python': set(),
+      '#!/usr/bin/env python':
+      {'/usr/lib/python3.6/site-packages/django/bin/django-admin.py',
+       '/usr/lib/python3.6/site-packages/django/conf/project_template/manage.py-tpl'}}),
+    ('pyserial-2.7-6.fc25.noarch.rpm',
+     {'#!/usr/bin/python': set(),
+      '#!/usr/bin/env python': set()}),
+))
+def test_get_scripts_summary(path, expected):
+    package = Package(fixtures_dir + path)
+    assert get_scripts_summary(package) == expected
